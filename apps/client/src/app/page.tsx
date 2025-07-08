d2f3d6e475e87f01fbb5e3a443f301097f3db621
wha1e <syh980130@gmail.com>
2025-07-08 16:10:50 +0900

    Revert "hotfix(domain): add hot fix landing"
    This reverts commit a6342e502ed2efa330af0f87187f858cc04512ea.

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -6,32 +6,14 @@ import ReviewSectionContainer from "@/containers/landing/ReviewSection";
 import { IntroSection } from "@/containers/landing/IntroSection";
 import { FAQSection } from "@/containers/landing/FAQSection";
 import { withAuth } from "@/components/HOC/withAuth";
-import Image from "next/image";
-import { Typography } from "@ui/components";
 
 function Home() {
   return (
     <CommonLayout isLogoVisible isFullWidth>
-      {/* <IntroSection />
+      <IntroSection />
       <DetailFunctions />
       <ReviewSectionContainer />
-      <FAQSection /> */}
-      <div className="flex flex-col items-center justify-center mt-10">
-        <Image
-          src="/assets/images/empty.png"
-          alt="빈 롤링페이퍼 이미지"
-          width={80}
-          height={80}
-          className="mb-4"
-        />
-        <Typography variant={"b20-medium"} className="mt-6 text-center">
-          긴급 업데이트 중이에요!
-          <br />
-          조금만 기다려달라 돌돌 ㅠㅠ
-          <br />
-          예상 완료 시각 : 17:00
-        </Typography>
-      </div>
+      <FAQSection />
     </CommonLayout>
   );
 }
